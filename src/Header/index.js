9231b6cbb079bfd9e7f32e2b7b378edf34229aa8
ERTHang <endrewrthang@gmail.com>
2019-08-03 00:04:40 -0300

    Update

--- src/Header/index.js
+++ src/Header/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './styles.css';
 
 const Header = () => (
-    <img src={require('../icon.png')} className = "Icon" />
+    <img src={require('../icon-removebg-preview.png')} className = "Icon" alt = "Dicert"/>
 );
 
 export default Header;
