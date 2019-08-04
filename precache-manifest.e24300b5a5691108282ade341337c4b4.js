8313c45c8d96b702a7a16380b269825478a46df4
ERTHang <endrewrthang@gmail.com>
2019-08-04 14:41:34 -0300

    Updates

--- precache-manifest.e24300b5a5691108282ade341337c4b4.js
+++ precache-manifest.e24300b5a5691108282ade341337c4b4.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "24cd51fa846cd6bf496fbc0faadbbdd7",
+    "revision": "20cbc2d380445691a6c7cdc36c017163",
     "url": "/Dicert-mini-project/index.html"
   },
   {
-    "revision": "024443bc54e32c86bf77",
-    "url": "/Dicert-mini-project/static/css/main.f903fd1b.chunk.css"
+    "revision": "6ba14dd920feb45d4664",
+    "url": "/Dicert-mini-project/static/css/main.84c9add1.chunk.css"
   },
   {
     "revision": "9eee67cf782a38d35aeb",
     "url": "/Dicert-mini-project/static/js/2.d6a534e5.chunk.js"
   },
   {
-    "revision": "024443bc54e32c86bf77",
+    "revision": "6ba14dd920feb45d4664",
     "url": "/Dicert-mini-project/static/js/main.93c4120c.chunk.js"
   },
   {
