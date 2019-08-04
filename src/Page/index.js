42a147d85d828d11a06c0669345b56ad34b73a2e
ERTHang <endrewrthang@gmail.com>
2019-08-04 14:13:09 -0300

    Update

--- src/Page/index.js
+++ src/Page/index.js
@@ -7,6 +7,7 @@ export default class Main extends Component{
         rndnum : 0,
         rndnum2 : 0,
         problem : 0,
+        notClickable : 1,
     }
 
 
@@ -16,10 +17,13 @@ export default class Main extends Component{
         this.setState({problem: ((25 * this.state.rndnum) + this.state.rndnum2) + 1000}, ()=>{
 
         });
+        this.setState({notClickable: 0});
     };
 
 
     render(){
+        const {problem} = this.state;
+
         return (
             <div>
             
@@ -29,7 +33,11 @@ export default class Main extends Component{
                 <ul className = "ul1">
                     <button className = "Button" onClick = {() => this.btnClick()}>ROLL</button>
                     <ul className = "ul2">
-                        <a target = "_blanck" className = "Button" href = {"https://www.urionlinejudge.com.br/judge/pt/problems/view/" + this.state.problem}>Acessar problema</a>
+                        {
+                            this.state.notClickable
+                            ? <a id = "dis" target = "_blanck" className = "Button" href = {"https://www.urionlinejudge.com.br/judge/pt/problems/view/" + this.state.problem}>Acessar problema</a>
+                            : <a target = "_blanck" className = "Button" href = {"https://www.urionlinejudge.com.br/judge/pt/problems/view/" + this.state.problem}>Acessar problema</a>
+                        }
                     </ul>
                 </ul>
             </div>
